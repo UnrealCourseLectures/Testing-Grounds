9ec5d94d8fa890619dc269a5f9449260a62974d7
Carlos <coky1594@gmail.com>
2018-11-12 01:12:26 -0400

    Real Commit -- Before Didn't Count

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,6 +2,21 @@
 
 #include "ChooseNextWaypoint.h"
 
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("WORKING!"));
+	return EBTNodeResult::Succeeded;
+}
 
+void UChooseNextWaypoint::GetPatrolPoints()
+{
+}
 
+int UChooseNextWaypoint::SetNextWaypoint()
+{
+	return 0;
+}
 
+void UChooseNextWaypoint::CycleNextIndex(int Index)
+{
+}
--- Source/TestingGrounds/ChooseNextWaypoint.h
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -14,7 +14,19 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
 	
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Patrol")
+	struct FBlackboardKeySelector Index;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Patrol")
+	struct FBlackboardKeySelector Waypoint_Location;
 	
-	
+	AActor* Local_PatrolPoints[4];
+
+private:
+	void GetPatrolPoints();
+	int SetNextWaypoint();
+	void CycleNextIndex(int Index);
 };
